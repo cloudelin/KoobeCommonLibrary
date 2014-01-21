51aa162a28acb893f345d27398297402cc08dbfa
cloude <cloude@630e58c9-a243-ca41-a344-d6b958b6d309>
2014-01-21 08:12:35 +0000

    git-svn-id: https://svn.koobe.com.tw:8443/svn/KoobeSystem3/KoobeCommonLibrary@1355 630e58c9-a243-ca41-a344-d6b958b6d309

--- KoobeCommonClient/src/test/java/com/koobe/common/client/KoobeClientServiceTest.java
+++ KoobeCommonClient/src/test/java/com/koobe/common/client/KoobeClientServiceTest.java
@@ -34,7 +34,7 @@ public class KoobeClientServiceTest {
 		assertNotNull(clientService);
 		
 		ExtractEpubRequest request = new ExtractEpubRequest("koobe-cloude-test", "koobe-cloude-test", 
-				"dickens-oliver-twist.epub", "unittest-" + System.currentTimeMillis());
+				"dickens-oliver-twist.epub", "KoobeClientServiceTest-" + System.currentTimeMillis());
 		
 		clientService.sendQueueMessageAndRunTask(request, "https://sqs.ap-southeast-1.amazonaws.com/417280986345/KGL-Service-Queue-Test");
 	}
